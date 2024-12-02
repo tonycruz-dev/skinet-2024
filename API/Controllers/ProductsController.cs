ce467b7cc725ddf14b0de6ce84a22760816f792c
Antonio Lopes da Cruz <tonycruz1@hotmail.com>
2024-12-02 11:22:07 +0000

    End of section 2

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController(StoreContext context) : ControllerBase
+{
+    //private readonly StoreContext _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        var products = await context.Products.ToListAsync();
+        return Ok(products);
+    }
+    [HttpGet("{id:int}")] // api/products/2
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await context.Products.FindAsync(id);
+
+        if (product == null) return NotFound();
+
+        return product;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(Product product)
+    {
+        context.Products.Add(product);
+
+        await context.SaveChangesAsync();
+
+        return product;
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateProduct(int id, Product product)
+    {
+        if (product.Id != id || !ProductExists(id))
+            return BadRequest("Cannot update this product");
+
+        context.Entry(product).State = EntityState.Modified;
+
+        await context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var product = await context.Products.FindAsync(id);
+
+        if (product == null) return NotFound();
+
+        context.Products.Remove(product);
+
+        await context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool ProductExists(int id)
+    {
+        return context.Products.Any(x => x.Id == id);
+    }
+}
